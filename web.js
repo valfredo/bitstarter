60822655bf0a32e4ef5c60e1cad1430a700d005a
Alfredo Villagome <valfredomx@gmail.com>
2013-07-06 07:34:34 -0700

    web.js

--- web.js
+++ web.js
@@ -5,10 +5,11 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
 var buffer = new Buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+  response.send(buffer.toString("utf-8", fs.readFileSync("index.html")));
  });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
