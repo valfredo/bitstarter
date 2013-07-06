13d6dd31b5c8a8a0dd4d67a3f38a8fa83752ab60
Alfredo Villagome <valfredomx@gmail.com>
2013-07-06 07:21:04 -0700

    Update web.js

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var fs = require('fs');
+var buffer = new Buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
