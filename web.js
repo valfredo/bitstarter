f01c5b0144ad1581b0cd170981d1f6db4b41b5d8
Alfredo Villagome <valfredomx@gmail.com>
2013-07-06 20:41:05 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
 var express = require('express');
+var fs - require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new Buffer ();
-  response.send(buffer.toString("utf-8", fs.readFileSync("index.html")));
- });
+  response.send(fs.readFileSync("index.html", 'utf8'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
