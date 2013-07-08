729c178a57f155a1858bd07a1697cf5add5bb043
Alfredo Villagome <valfredomx@gmail.com>
2013-07-08 07:37:20 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
-var fs - require('fs');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html", 'utf8'));
+    var index_file_data = fs.readFileSync('index.html');
+    response.send(index_file_data.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
 
