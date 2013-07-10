ec80998d613ab00a200a037f021a073c8ecaf8b6
Alfredo Villagome <valfredomx@gmail.com>
2013-07-10 10:16:54 -0700

    Updated files

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,10 +62,19 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var download_url_content = function(url, checksfile) {
+    rest.get(url).on('success', function(data, response) {
+        var checkJson = checkHtmlFile(data, checksfile);
+        var outJson = JSON.stringify(checkJson, null, 4);
+    });
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Path to URL')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
