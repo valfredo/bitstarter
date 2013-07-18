d499c3fac445a5d0ba6281f391470a3b47c1fdce
valfredo <valfredomx@gmail.com>
2013-07-18 17:20:32 +0000

    Updated the web.js file

--- web.js
+++ web.js
@@ -8,8 +8,8 @@ app.get('/', function(request, response) {
     response.send(index_file_data.toString());
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+var port = process.env.PORT || 8080;
+ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
